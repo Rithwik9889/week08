9afebf17749b233d7d7ff67bb67b8c2f09e9847b
Rithwik9889 <pokarithwik@gmail.com>
2026-09-25 23:53:26 +1000

    Add CI/CD pipeline and update frontend heading

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -93,7 +93,7 @@ const Login = () => {
             variant="h4"
             gutterBottom
           >
-            KoalaTech University
+                        KoalaTech University - V2 Deployed
           </Typography>
 
           <Typography
